c6a828d80d3c84b66bd021c2003c2846ce3cde90
Chaudhary Hamza <chaudharymhamza2006@gmail.com>
2025-08-26 15:29:08 +0500

    I have completed Module 01 and Module 02

--- Module02/Task06.py
+++ Module02/Task06.py
@@ -1,7 +1,7 @@
 import random
 
-random01 = random.randint(0, 9)
-random02 = random.randint(0, 9)
-random03 = random.randint(0, 9)
+code3 = "".join(str(random.randint(0, 9)) for _ in range(3))
+code4 = "".join(str(random.randint(1, 6)) for _ in range(4))
 
-print(f"{random01}{random02}{random03}")
+print("3-digit lock code:", code3)
+print("4-digit lock code:", code4)
